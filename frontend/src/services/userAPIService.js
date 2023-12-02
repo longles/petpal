11681b3bb33186129fd823674ef5732d12e5047b
longles <lingxiaoai@gmail.com>
2023-12-02 01:37:29 -0500

    added api calls for seekers/shelters

---
+++ frontend/src/services/userAPIService.js
@@ -0,0 +1,134 @@
+import { APIService } from './APIService.js';
+
+export const seekerAPIService = () => {
+    const API_PATH = 'accounts/seekers/';
+    const apiService = APIService();
+
+    const getSeekerDetail = async (id) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'GET');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const updateSeeker = async (id, to_update) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'PUT', to_update);
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const deleteSeeker = async (id) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'DELETE');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+        }
+    }
+
+    return {
+        getSeekerDetail,
+        updateSeeker,
+        deleteSeeker,
+    }
+}
+
+export const shelterAPIService = () => {
+    const API_PATH = 'accounts/shelters/';
+    const apiService = APIService();
+
+    const getShelterDetail = async (id) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'GET');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const getShelterList = async (filters, page) => {
+        const response = await apiService.makeAPICall(`${API_PATH}?${filters}&page=${page}`, 'GET');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const updateShelter = async (id, to_update) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'PUT', to_update);
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const deleteShelter = async (id) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'DELETE');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+        }
+    }
+
+    return {
+        getShelterDetail,
+        getShelterList,
+        updateShelter,
+        deleteShelter,
+    }
+}
