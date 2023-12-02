db5745cee6ecf91d068bdee4d5f3cdbccc18b773
longles <lingxiaoai@gmail.com>
2023-12-02 01:15:06 -0500

    added api calls for pet

---
+++ frontend/src/services/petAPIService.js
@@ -0,0 +1,99 @@
+import { APIService } from "./APIService";
+
+export const petAPIService = () => {
+    const API_PATH = 'pets/';
+    const apiService = APIService();
+
+    // All other fields can be null; include in other_info as required
+    const createPet = async (name, other_info) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}`, 'POST', {
+            name: name,
+            ...other_info,
+        });
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const getPetDetail = async (id) => {
+        const response = await apiService.makeAPICall(`${API_PATH}${id}/`, 'GET');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const getPetList = async (filters, page) => {
+        const response = await apiService.makeAPICall(`${API_PATH}?${filters}&page=${page}`, 'GET');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const updatePet = async (id, to_update) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'PUT', to_update);
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+            data: response.data,
+        }
+    }
+
+    const deletePet = async (id) => {
+        const response = await apiService.makePrivateAPICall(`${API_PATH}${id}/`, 'DELETE');
+
+        if (!response.success) {
+            return {
+                success: false,
+                message: response.detail,
+            }
+        }
+
+        return {
+            success: true,
+        }
+    }
+
+    return {
+        createPet,
+        getPetDetail,
+        getPetList,
+        updatePet,
+        deletePet,
+    }
+}
+
+// Maybe add utils for generating pet field jsons or return all field names
