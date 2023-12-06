455b2ac703c5adadf859fe9a1f436157611d8c26
bw55555 <brianwang2013@gmail.com>
2023-12-06 13:47:28 -0500

    Initial commit with UI

--- frontend/src/components/application/ApplicationFormUpdateModal.jsx
+++ frontend/src/components/application/ApplicationFormUpdateModal.jsx
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useReducer, useMemo, useCallback } from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import FormQuestion from './FormQuestion';
 
 const ApplicationFormUpdateModal = ({id, initialQuestions}) => {
-    const [showModal, setShowModal] = useState(true)
-    const [questions, setQuestions] = useState([
+    initialQuestions = [
         {
             "id": 4,
             "title": "How much do you earn?",
@@ -54,16 +53,61 @@ const ApplicationFormUpdateModal = ({id, initialQuestions}) => {
                 ]
             }
         }
-    ])
+    ]
+
+    const questionReducer = (questions, action) => {
+        console.log(action)
+        const {type, idx, question} = action
+        console.log("idk why this was called")
+        if (type === 'add') {
+            return [...questions, question]
+        }
+        if (type === 'update') {
+            return questions.map((x, i) => i === idx ? question : x)
+        }
+        if (type === 'delete') {
+            return questions.filter((x, i) => i !== idx)
+        }
+        throw Error('Unknown action: ' + type);
+    }
+
+    const [questions, dispatch] = useReducer(questionReducer, initialQuestions)
+
+    const addQuestion = () => {
+        dispatch({type: "add", question: {title: "New Question", question_object: {type: 1, prompt: []}}})
+    }
+
+    const editQuestionByIndex = useCallback((idx) => {
+        return (question) => {
+            console.log(question)
+            dispatch({type: "update", idx: idx, question: question})
+        }
+    }, [questions])
+
+    const deleteQuestionByIndex = useCallback((idx) => {
+        return () => {dispatch({type: "delete", idx: idx})}
+    }, [questions])
+
+    const [showModal, setShowModal] = useState(true)
+    
+    console.log(questions)
     return (
         <Modal show={showModal} onHide={() => {setShowModal(false)}} size="lg">
             <Modal.Header closeButton>
                 <Modal.Title>Pet Details</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                {questions.map(x => <FormQuestion edit={true} question={x}/>)}
+                {questions.map((x, i) => <FormQuestion key={i} edit={true} title={x.title} question={x.question_object} editFunc={editQuestionByIndex(i)} deleteFunc={deleteQuestionByIndex(i)}/>)}
             </Modal.Body>
-            <button className='btn btn-primary'>Add new question</button>
+            <div className="d-flex justify-content-center mb-2">
+                <button className='btn btn-primary' onClick={addQuestion}>Add new question</button>
+            </div>
+            <Modal.Footer>
+                <button className='btn btn-primary' onClick={addQuestion}>Save</button>
+            </Modal.Footer>
+            
+
+            
         </Modal>
         
     )
