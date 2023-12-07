c15bf5596ee7f6707e2cd726217d4ad791f03b4c
Zheng Chen <chenzheng0430@gmail.com>
2023-12-07 04:06:57 -0500

    shelter filter enabled

--- frontend/src/components/listing/SideBarFilter.jsx
+++ frontend/src/components/listing/SideBarFilter.jsx
@@ -1,27 +1,97 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import FilterRow from './FilterRow';
+import { shelterAPIService } from '../../services/userAPIService';
 
 const SideBarFilter = ({ updateFilters }) => {
+
+  const shelterAPI = shelterAPIService();
   const statusOptions = ['Any', 'Adopted', 'Available'];
   const breedOptions = ['Any', 'Ragdoll', 'Labrador', 'Parrot'];
   const sizeOptions = ['Any', 'Small', 'Medium', 'Large'];
   const colourOptions = ['Any', 'Yellow', 'Black', 'White', 'Brown', 'Grey', 'Red', 'Blue', 'Green'];
   const sexOptions = ['Any', 'Male', 'Female'];
+  const [shelterOptions, setShelterOptions] = useState(['Any']);
+  const [filterValueMap, setFilterValueMap] = useState({
+    status: {
+      adopted: 1,
+      available: 2,
+    },
+    sex: {
+      unknown: 0,
+      male: 1,
+      female: 2,
+    },
+    size: {
+      large: 1,
+      medium: 2,
+      small: 3,
+    },
+    colour: {
+      unknown: 0,
+      yellow: 1,
+      black: 2,
+      white: 3,
+      brown: 4,
+      grey: 5,
+      red: 6,
+      blue: 7,
+      green: 8,
+    },
+    species: {
+      unknown: 0,
+      dog: 1,
+      cat: 2,
+      bird: 3,
+    },
+    breed: {
+      unknown: '0',
+      ragdoll: '1',
+      labrador: '2',
+      parrot: '3',
+    },
+    shelter: {}
+  });
+
+  useEffect(() => {
+    const fetchShelterList = async () => {
+      try {
+        const response = await shelterAPI.getShelterList(1);
+        if (response.success) {
+          const updatedShelterOptions = ['Any'];
+          const shelterMap = {};
+
+          response.data.results.forEach((shelter) => {
+            updatedShelterOptions.push(shelter.shelter_name);
+            shelterMap[shelter.shelter_name.toLowerCase()] = shelter.id;
+          });
+
+          setShelterOptions(updatedShelterOptions);
+          setFilterValueMap((prevMap) => ({ ...prevMap, shelter: shelterMap }));
+        }
+      } catch (error) {
+        console.error('Error fetching shelter list:', error);
+      }
+    };
+
+    fetchShelterList();
+  }, []);
 
   const handleFilterSubmit = (event) => {
-    event.preventDefault(); // Prevents the default form submission
-  
-  
+    event.preventDefault();
     const form = event.target;
     const formData = new FormData(form);
     const selectedFilters = {};
 
     for (let [key, value] of formData.entries()) {
       if (value !== 'Any') {
-        selectedFilters[key] = value;
+        if (filterValueMap[key]) {
+          const selectedValue = value.toLowerCase();
+          selectedFilters[key] = filterValueMap[key][selectedValue];
+        } else {
+          selectedFilters[key] = value;
+        }
       }
     }
-    console.log('Selected Filters:', selectedFilters);
     updateFilters(selectedFilters);
   };
 
@@ -58,6 +128,9 @@ const SideBarFilter = ({ updateFilters }) => {
 
             {/* Sex filter */}
             <FilterRow name="Sex" options={sexOptions} />
+
+            {/* shelter filter */}
+            <FilterRow name="Shelter" options={shelterOptions} />
 
             <button type="submit" className="btn btn-primary">Apply Filters</button>
           </form>
