cf0c711e2cc6bc605b85c811eae22afa4d1cd85c
vesteny77 <yvesten_25e@icloud.com>
2023-12-08 11:27:23 -0500

    modify edit profile shelter

--- frontend/src/components/profile/EditProfileShelter.jsx
+++ frontend/src/components/profile/EditProfileShelter.jsx
@@ -68,42 +68,43 @@ const EditProfileShelter = ({id}) => {
             <form onSubmit={handleProfileSubmit}>
                 <div className="form-group">
                     <label htmlFor="username">Shelter Name</label>
-                    <input type="text" className="form-control" id="username" placeholder="Username"
-                           value="Animal Shelter"/>
+                    <input type="text" className="form-control" id="username" placeholder="Name of your shelter"
+                           value={profileData.shelterName} onChange={handleProfileChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="location">Shelter Location</label>
                     <input type="text" className="form-control" id="location"
-                           value="This, That, and The Other Streets"/>
+                           value={profileData.location} onChange={handleProfileChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="mission">Mission Statement</label>
-                    <textarea className="form-control" id="mission" rows="3">{profileData.missionStatement}</textarea>
+                    <textarea className="form-control" id="mission" rows="3"
+                              value={profileData.missionStatement} onChange={handleProfileChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="aboutUsProfile">About Us</label>
-                    <textarea className="form-control" id="aboutUsProfile" rows="3">
-                        {profileData.aboutUs}
-                    </textarea>
+                    <textarea className="form-control" id="aboutUsProfile" rows="3"
+                              value={profileData.aboutUs} onChange={handleProfileChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email" aria-describedby="emailHelp">Email</label>
                     <input type="text" className="form-control" id="email" placeholder="email"
-                           value={profileData.email}/>
+                           value={profileData.email} onChange={handleProfileChange} />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone
                         else.</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="mobile">Mobile</label>
                     <input type="text" className="form-control" id="mobile" placeholder="xxx-xxx-xxxx"
-                           value={profileData.mobile}
+                           value={profileData.mobile} onChange={handleProfileChange}
                     />
                     <small className="form-text text-muted">We'll never share your phone number with anyone
                         else.</small>
                 </div>
                 <div className="form-group">
                     <label htmlFor="profilePic">Change Shelter Pic</label>
-                    <input type="file" className="form-control" id="profilePic" value={profileData.profilePic}/>
+                    <input type="file" className="form-control" id="profilePic"
+                           value={profileData.profilePic} onChange={handleProfileChange} />
                 </div>
                 <Link to="#" className="btn btn-primary" id="showAlert">Save</Link>
                 <Link to="/profile_shelter" id="cancel" className="btn btn-dark">Discard Changes</Link>
@@ -115,13 +116,15 @@ const EditProfileShelter = ({id}) => {
             <form onSubmit={handleSecuritySubmit}>
                 <div className="form-group">
                     <label htmlFor="newPassword">New Password</label>
-                    <input type="password" className="form-control" id="newPassword1" value="NewPassword"/>
+                    <input type="password" className="form-control" id="newPassword1"
+                           value={securityData.newPassword1} onChange={handleSecurityChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="newPassword2">Confirm New Password</label>
-                    <input type="password" className="form-control" id="newPassword2" value="NewPassword"/>
+                    <input type="password" className="form-control" id="newPassword2"
+                           value={securityData.newPassword2} onChange={handleSecurityChange} />
                 </div>
-                <a href="#" className="btn btn-primary" id="showAlert2">Submit</a>
+                <button className="btn btn-primary" id="showAlert2">Submit</button>
             </form>
         </div>
     )
