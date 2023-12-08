93c3a2573277ed550f3b5ad212f3886232dce44b
longles <lingxiaoai@gmail.com>
2023-12-08 14:17:51 -0500

    working ui, broken api call

--- frontend/src/components/shared/ApplicationModal.jsx
+++ frontend/src/components/shared/ApplicationModal.jsx
@@ -1,61 +1,171 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import { applicationFormAPIService } from '../../services/applicationFormAPIService';
+import { applicationAPIService } from '../../services/applicationAPIService';
 
-const ApplicationModal = ({ closeModal, show }) => {
-  return (
-    <Modal show={show} onHide={closeModal} size="lg">
-      <Modal.Header closeButton>
-        <h3 className="modal-title" id="adoptionFormModalLabel">Your Application</h3>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Row>
-            <Col md={6}>
-              <Form.Group controlId="firstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" required />
-              </Form.Group>
-            </Col>
-            <Col md={6}>
-              <Form.Group controlId="lastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" required />
-              </Form.Group>
-            </Col>
-          </Row>
+const ApplicationModal = ({ closeModal, show, petId, formId }) => {
+    const [questions, setQuestions] = useState([]);
+    const [responses, setResponses] = useState({});
+    const applicationFormService = applicationFormAPIService();
+    const applicationService = applicationAPIService();
 
-          <Form.Group controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" required />
-          </Form.Group>
+    useEffect(() => {
+        const fetchQuestions = async () => {
+            const response = await applicationFormService.getApplicationFormDetail(formId);
+            if (response.success) {
+                setQuestions(response.data.questions);
+            } else {
+                console.error('Error fetching application form:', response.message);
+            }
+        };
 
-          <Form.Group controlId="address">
-            <Form.Label>Address</Form.Label>
-            <Form.Control type="text" required />
-          </Form.Group>
+        fetchQuestions();
+    }, [formId]);
 
-          <Form.Group controlId="peopleAtHome">
-            <Form.Label>How many people live at home?</Form.Label>
-            <Form.Control type="number" min="1" required />
-          </Form.Group>
+    // Doesn't really work...
 
-          <Form.Group controlId="reasonForAdoption">
-            <Form.Label>Why do you want to adopt this pet?</Form.Label>
-            <Form.Control as="textarea" rows={4} required />
-          </Form.Group>
+    const handleChange = (questionId, type) => (e) => {
+        if (type === 4) {
+            // Handling checkboxes
+            const updatedResponses = responses[questionId] ? [...responses[questionId].response] : [];
+            if (e.target.checked) {
+                updatedResponses.push(e.target.value);
+            } else {
+                const index = updatedResponses.indexOf(e.target.value);
+                if (index > -1) {
+                    updatedResponses.splice(index, 1);
+                }
+            }
+            setResponses(prevResponses => ({
+                ...prevResponses,
+                [questionId]: { type, response: updatedResponses }
+            }));
+        } else {
+            // Handling other input types
+            setResponses(prevResponses => ({
+                ...prevResponses,
+                [questionId]: { type, response: e.target.value }
+            }));
+        }
+    };
 
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={closeModal}>
-          Back
-        </Button>
-        <Button variant="primary" id="showAlert">
-          Submit
-        </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+    // Rendering works, but looks ugly
+
+    const renderInputForm = (question) => {
+        const { id, type, prompt } = question.question_object;
+
+        switch (type) {
+            case 1: // Textarea
+                return (
+                    <Form.Control
+                        as="textarea"
+                        rows={4}
+                        onChange={handleChange(id, type)}
+                        required
+                    />
+                );
+
+            case 2: // Dropdown
+                return (
+                    <Form.Select onChange={handleChange(id, type)} required>
+                        {prompt.map((option, index) => (
+                            <option key={index} value={option}>{option}</option>
+                        ))}
+                    </Form.Select>
+                );
+
+            case 3: // Radio
+                return (
+                    <div>
+                        {prompt.map((option, index) => (
+                            <Form.Check
+                                key={index}
+                                type="radio"
+                                label={option}
+                                name={id}
+                                id={`${id}-${index}`}
+                                onChange={handleChange(id, type)}
+                            />
+                        ))}
+                    </div>
+                );
+
+            case 4: // Checkbox
+                return (
+                    <div>
+                        {prompt.map((option, index) => (
+                            <Form.Check
+                                key={index}
+                                type="checkbox"
+                                label={option}
+                                name={id}
+                                id={`${id}-${index}`}
+                                value={option}
+                                onChange={handleChange(id, type)}
+                            />
+                        ))}
+                    </div>
+                );
+
+            case 5: // File
+                return (
+                    <Form.Control
+                        type="file"
+                        onChange={handleChange(id, type)}
+                    />
+                );
+
+            default:
+                return null;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // Formatting responses for submission
+        const formattedResponses = Object.entries(responses).map(([questionId, responseObj]) => ({
+            question: parseInt(questionId),
+            response_object: responseObj
+        }));
+
+        const requestBody = {
+            pet: petId,
+            form: formId,
+            responses: formattedResponses
+        };
+
+        const response = await applicationService.createApplication(requestBody);
+
+        if (response.success) {
+            closeModal();
+        } else {
+            console.error('Error submitting application:', response.message);
+        }
+    };
+
+    return (
+        <Modal show={show} onHide={closeModal} size="lg">
+            <Modal.Header closeButton>
+                <h3 className="modal-title">Your Application</h3>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    {questions.map((question) => (
+                        <Form.Group as={Row} controlId={question.id} key={question.id}>
+                            <Form.Label column mb="3">{question.title}</Form.Label>
+                            <Col sm="9">
+                                {renderInputForm(question)}
+                            </Col>
+                        </Form.Group>
+                    ))}
+                    <Button type="submit" variant="primary">Submit</Button>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={closeModal}>Back</Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default ApplicationModal;
--- frontend/src/components/shared/PetCard.jsx
+++ frontend/src/components/shared/PetCard.jsx
@@ -2,86 +2,88 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { petAPIService } from '../../services/petAPIService';
 import PetDetailsModal from './PetDetailsModal';
 import ApplicationModal from './ApplicationModal';
-import PetUpdateModal, {getUpdateModalId} from './PetUpdateModal';
+import PetUpdateModal, { getUpdateModalId } from './PetUpdateModal';
 
-const PetCard = ({ manageFlag = false, petId }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isApplicationModalOpen, setIsApplicationModalOpen] = useState(false);
-  const [petDetails, setPetDetails] = useState(null);
+const PetCard = ({ manageFlag = false, petId, formId }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isApplicationModalOpen, setIsApplicationModalOpen] = useState(false);
+    const [petDetails, setPetDetails] = useState(null);
 
-  const fetchPetDetail = useCallback(async () => {
-    try {
-      const petAPI = petAPIService();
-      const response = await petAPI.getPetDetail(petId);
-      if (response.success) {
-        setPetDetails(response.data);
-      }
-    } catch (error) {
-      console.error(`Error fetching pet detail for ID ${petId}:`, error);
+    const fetchPetDetail = useCallback(async () => {
+        try {
+            const petAPI = petAPIService();
+            const response = await petAPI.getPetDetail(petId);
+            if (response.success) {
+                setPetDetails(response.data);
+            }
+        } catch (error) {
+            console.error(`Error fetching pet detail for ID ${petId}:`, error);
+        }
+    }, [petId]);
+
+    useEffect(() => {
+        fetchPetDetail();
+    }, [fetchPetDetail]);
+
+    if (!petDetails) {
+        return <div>Loading...</div>;
     }
-  }, [petId]);
 
-  useEffect(() => {
-    fetchPetDetail();
-  }, [fetchPetDetail]);
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
 
-  if (!petDetails) {
-    return <div>Loading...</div>;
-  }
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
 
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
+    const openApplicationModal = () => {
+        setIsApplicationModalOpen(true);
+    };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+    const closeApplicationModal = () => {
+        setIsApplicationModalOpen(false);
+    };
 
-  const openApplicationModal = () => {
-    setIsApplicationModalOpen(true);
-  };
-
-  const closeApplicationModal = () => {
-    setIsApplicationModalOpen(false);
-  };
-
-  const photo = petDetails.photo === null ? '/assets/images/sample_pet_image_2.jpg' : petDetails.photo;
-  return (
-    <div className="col-md-4 mb-4 d-flex align-items-stretch">
-      <div className="card listing-box">
-        <img src={photo} className="card-img-top" alt={petDetails.name} />
-        <div className="card-body">
-          <h4 className="card-title">{petDetails.name}</h4>
-          <p className="card-text">{petDetails.comments}</p>
-          <div>
-            {!manageFlag && <>
-              <button className="btn btn-primary" onClick={openModal}>
-                Details
-              </button>
-              {isModalOpen && (
-                <PetDetailsModal
-                  petId={petId}
-                  closeModal={closeModal}
-                  openApplicationModal={openApplicationModal}
+    const photo = petDetails.photo === null ? '/assets/images/sample_pet_image_2.jpg' : petDetails.photo;
+    return (
+        <div className="col-md-4 mb-4 d-flex align-items-stretch">
+            <div className="card listing-box">
+                <img src={photo} className="card-img-top" alt={petDetails.name} />
+                <div className="card-body">
+                    <h4 className="card-title">{petDetails.name}</h4>
+                    <p className="card-text">{petDetails.comments}</p>
+                    <div>
+                        {!manageFlag && <>
+                            <button className="btn btn-primary" onClick={openModal}>
+                                Details
+                            </button>
+                            {isModalOpen && (
+                                <PetDetailsModal
+                                    petId={petId}
+                                    closeModal={closeModal}
+                                    openApplicationModal={openApplicationModal}
+                                />
+                            )}
+                        </>}
+                        {manageFlag && <>
+                            <button className="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target={"#" + getUpdateModalId(petId)}>Edit</button>
+                            <PetUpdateModal petId={petId} />
+                        </>}
+                    </div>
+                </div>
+            </div>
+            {isApplicationModalOpen && (
+                <ApplicationModal
+                    show={isApplicationModalOpen}
+                    closeModal={closeApplicationModal}
+                    petId={petId}
+                    formId={formId}
                 />
-              )}
-            </>}
-            {manageFlag && <>
-              <button className="btn btn-primary" data-bs-toggle="modal"
-                                    data-bs-target={"#"+getUpdateModalId(petId)}>Edit</button>
-              <PetUpdateModal petId={petId}/>
-            </>}
-          </div>
+            )}
         </div>
-      </div>
-      {isApplicationModalOpen && (
-        <ApplicationModal
-          show={isApplicationModalOpen}
-          closeModal={closeApplicationModal}
-        />
-      )}
-    </div>
-  );
+    );
 };
 
 export default PetCard;
