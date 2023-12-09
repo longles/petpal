333ce96ab391aad809b2aece345939c6ce7b301c
Zheng Chen <chenzheng0430@gmail.com>
2023-12-09 13:07:58 -0500

    detail button always at bottom now

--- frontend/src/components/shared/PetCard.jsx
+++ frontend/src/components/shared/PetCard.jsx
@@ -50,13 +50,14 @@ const PetCard = ({ manageFlag = false, petId, data }) => {
     const photo = petDetails.photo === null ? '/assets/images/sample_pet_image_2.jpg' : petDetails.photo;
     return (
         <div className="col-md-4 mb-4 d-flex align-items-stretch">
-            <div className="card listing-box">
-                <img src={photo} className="card-img-top" alt={petDetails.name} />
-                <div className="card-body">
-                    <h4 className="card-title">{petDetails.name}</h4>
-                    <p className="card-text">{petDetails.comments}</p>
-                    <div>
-                        {!manageFlag && <>
+        <div className="card d-flex flex-column h-100">
+            <img src={photo} className="card-img-top pet-photo" alt={petDetails.name} />
+            <div className="card-body d-flex flex-column">
+                <h4 className="card-title">{petDetails.name}</h4>
+                <p className="card-text">{petDetails.comments}</p>
+                <div className="mt-auto">
+                    {!manageFlag && (
+                        <>
                             <button className="btn btn-primary" onClick={openModal}>
                                 Details
                             </button>
@@ -67,25 +68,28 @@ const PetCard = ({ manageFlag = false, petId, data }) => {
                                     openApplicationModal={openApplicationModal}
                                 />
                             )}
-                        </>}
-                        {manageFlag && <>
+                        </>
+                    )}
+                    {manageFlag && (
+                        <>
                             <button className="btn btn-primary" data-bs-toggle="modal"
                                 data-bs-target={"#" + getUpdateModalId(petId)}>Edit</button>
                             <PetUpdateModal petId={petId} />
-                        </>}
-                    </div>
+                        </>
+                    )}
                 </div>
             </div>
-            {isApplicationModalOpen && (
-                <ApplicationModal
-                    show={isApplicationModalOpen}
-                    closeModal={closeApplicationModal}
-                    petId={petId}
-                    formId={petDetails.form}
-                    shelterId={petDetails.shelter}
-                />
-            )}
         </div>
+        {isApplicationModalOpen && (
+            <ApplicationModal
+                show={isApplicationModalOpen}
+                closeModal={closeApplicationModal}
+                petId={petId}
+                formId={petDetails.form}
+                shelterId={petDetails.shelter}
+            />
+        )}
+    </div>
     );
 };
 
