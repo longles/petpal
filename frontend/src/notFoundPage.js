3e6beb0e8ccea6bdc61374b007406b77369241cc
vesteny77 <yvesten_25e@icloud.com>
2023-12-09 16:38:11 -0500

    add general 404 page

---
+++ frontend/src/notFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => (
+  <div>
+    <h1>404 - Page Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+  </div>
+);
+
+export default NotFoundPage;
