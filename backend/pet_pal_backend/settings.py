046355798792aa1e7a2c8312d6513a5161478213
longles <lingxiaoai@gmail.com>
2023-12-11 00:21:52 -0500

    ec2 fix

--- backend/pet_pal_backend/settings.py
+++ backend/pet_pal_backend/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fz75a(qww$@2+s6)megwrk4xlmm^@vzck!skjbqhwmfzw-6nt8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
